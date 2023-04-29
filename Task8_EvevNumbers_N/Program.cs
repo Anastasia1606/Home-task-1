9636a31505c69659efda2190819be349836b38e6
Anastasia <nastya.arkanova.98@bk.ru>
2023-04-29 15:02:25 +0500

    add Task8_EvenNumbers_N

---
+++ Task8_EvevNumbers_N/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+int count = 2;
+string result;
+while (count <= number1)
+{
+  Console.Write(count + " ");
+  count = count + 2;
+}
